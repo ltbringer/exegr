0fde1201205ce0dea737364047dbccd1d5f30584
ltbringer <amresh.venugopal@gmail.com>
2020-05-30 03:02:58 +0530

    add: span_matrix method to return a matrix of size (M, N).
    Here M is the number of patterns and N is the number of strings
    to pattern match. Elements in the matrix could be a Tuple of two usize
    elements or None.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@ extern crate regex;
 
 use pyo3::prelude::*;
 use std::os::raw::c_double;
-use regex::Regex;
+use regex::{Regex};
 
 
 #[pyclass]
 struct GroupRegexMatch {
   compiled_patterns: Vec<Regex>
 }
+
 
 #[pymethods]
 impl GroupRegexMatch {
@@ -46,6 +47,21 @@ impl GroupRegexMatch {
         let score_sigmoid: f64 = if non_zero_scores.len() > 0 { 1. / (1. + e_to_score) } else { 0. };
         score_sigmoid
       }).collect())
+  }
+
+  fn span_matrix(&self, texts: Vec<&str>) -> PyResult<Vec<Vec<Option<(usize, usize)>>>> {
+    let mut span_matrix: Vec<Vec<Option<(usize, usize)>>> = Vec::with_capacity(self.compiled_patterns.len());
+    for pattern in self.compiled_patterns.iter() {
+      let mut span_vector: Vec<Option<(usize, usize)>> = Vec::with_capacity(texts.len());
+      for text in texts.iter() {
+        match pattern.find(text) {
+          None => span_vector.push(None),
+          Some(mat) => span_vector.push(Some((mat.start(), mat.end())))
+        };
+      }
+      span_matrix.push(span_vector);
+    }
+    Ok(span_matrix)
   }
 }
 
